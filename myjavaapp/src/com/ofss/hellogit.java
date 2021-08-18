627f8ed3f76ae67ce0ed0a809773c88375d3027c
Harinee <harineedevidamodaran@gmail.com>
2021-08-18 16:26:49 +0530

    "second commit of one method"

--- myjavaapp/src/com/ofss/hellogit.java
+++ myjavaapp/src/com/ofss/hellogit.java
@@ -2,9 +2,15 @@ package com.ofss;
 
 public class hellogit {
 
+	public static int add(int a,int b)
+	{
+		return a+b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("hrllo git");
+		System.out.println(add(5,6));
 
 	}
 
